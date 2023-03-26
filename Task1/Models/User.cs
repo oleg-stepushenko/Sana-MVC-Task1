1bf46b7fdd2d8825ef05fd7c69816121a749fd02
Oleg Stepushenko <oleg16stepushenko@gmail.com>
2023-03-26 22:02:00 +0300

    update Layout

--- Task1/Models/User.cs
+++ Task1/Models/User.cs
@@ -1,6 +1,11 @@
-﻿namespace Task1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Task1.Models
 {
     public class User
     {
+        [Required]
+        public string Name { set; get; }
     }
 }
+
